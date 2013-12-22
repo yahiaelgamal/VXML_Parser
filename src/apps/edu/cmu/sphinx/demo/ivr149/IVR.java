df2f04132d61f212ed9ec095eecc159ca8a12516
DevYah <yahiaelgamal@gmail.com>
2013-12-22 22:39:30 +0200

    Enahnce readibility

--- src/apps/edu/cmu/sphinx/demo/ivr149/IVR.java
+++ src/apps/edu/cmu/sphinx/demo/ivr149/IVR.java
@@ -29,8 +29,7 @@ import java.io.*;
 
 public class IVR {
 
-      public static int price(String size, String toppings, int count, String crust)
-      {
+    public static int price(String size, String toppings, int count, String crust) {
         int res = 20;
         if (size.equals("big")) res += 5;
         if (size.equals("regular")) res += 3;
@@ -41,13 +40,13 @@ public class IVR {
         if (crust.equals("pan")) res += 3;
         if (crust.equals("thick")) res += 5;
         return res*count;
-      }
+    }
 
-    public static ArrayList<Field> parseXMLFile(String fileName){
+    public static ArrayList<Field> parseXMLFile(String fileName) {
         ArrayList<Field> fields = new ArrayList<Field>();
         try {
             File fXmlFile = new File(fileName);
-            DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilderFactory dbFactory = DocumentBuilderFactory .newInstance();
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document dialog = dBuilder.parse(fXmlFile);
 
@@ -55,27 +54,30 @@ public class IVR {
 
             NodeList nList = dialog.getElementsByTagName("field");
 
-
             for (int i = 0; i < nList.getLength(); i++) {
 
                 Node nNode = nList.item(i);
-
                 Field f;
-
                 Element eElement = (Element) nNode;
 
                 String prompt;
-                if(eElement.getElementsByTagName("prompt").item(0).getChildNodes().getLength() > 1)
-                    prompt = eElement.getElementsByTagName("prompt").item(0).getFirstChild().getTextContent() + " <insert_item> "
-                    + eElement.getElementsByTagName("prompt").item(0).getLastChild().getTextContent();
+                if (eElement.getElementsByTagName("prompt").item(0)
+                        .getChildNodes().getLength() > 1)
+                    prompt = eElement.getElementsByTagName("prompt").item(0)
+                            .getFirstChild().getTextContent()
+                            + " <insert_item> "
+                            + eElement.getElementsByTagName("prompt").item(0)
+                                    .getLastChild().getTextContent();
                 else
-                    prompt = eElement.getElementsByTagName("prompt").item(0).getTextContent();
+                    prompt = eElement.getElementsByTagName("prompt").item(0)
+                            .getTextContent();
 
                 String filled = null;
-                if(eElement.getElementsByTagName("filled").item(0) != null)
-                    filled = eElement.getElementsByTagName("filled").item(0).getTextContent();
+                if (eElement.getElementsByTagName("filled").item(0) != null)
+                    filled = eElement.getElementsByTagName("filled").item(0)
+                            .getTextContent();
 
-                if(filled == null)
+                if (filled == null)
                     filled = " ";
 
                 String name = eElement.getAttribute("name");
@@ -85,7 +87,7 @@ public class IVR {
                 NodeList itemList = eElement.getElementsByTagName("item");
 
                 for (int j = 0; j < itemList.getLength(); j++) {
-                    if(itemList.item(j) != null)
+                    if (itemList.item(j) != null)
                         f.options.add(itemList.item(j).getTextContent());
                 }
                 fields.add(f);
@@ -97,69 +99,71 @@ public class IVR {
             e.printStackTrace();
             return null;
         }
-   }
+    }
 
-   public static void main(String[] args) throws IOException {
-      {
-          String fileName = "dialog1.xml";
-          ArrayList<Field> fields = parseXMLFile(fileName);
+    public static void main(String[] args) throws IOException {
+        String fileName = "dialog1.xml";
+        ArrayList<Field> fields = parseXMLFile(fileName);
 
+        String size = null;
+        String toppings = null;
+        String crust = null;
+        String count = null;
 
-          String size = null;
-          String toppings = null;
-          String crust = null;
-          String count = null;
+        System.out.println("Welcome to your pizza ordering service!");
 
-          System.out.println("Welcome to your pizza ordering service!");
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String answer = "";
 
-          BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-          String answer = "";
-          for(int i = 0; i < fields.size(); i++){
-              Field curField = fields.get(i);
-              if(!curField.name.startsWith("confirm")){
-                  answer = null;
-                  while(answer == null){
-                      // what the system says
-                      System.out.println(curField.prompt);
-                      // the answer you get from the user
-                      answer = bf.readLine();
+        for (int i = 0; i < fields.size(); i++) {
+            Field curField = fields.get(i);
+            if (!curField.name.startsWith("confirm")) {
+                answer = null;
+                while (answer == null) {
+                    // what the system says
+                    System.out.println(curField.prompt);
+                    // the answer you get from the user
+                    answer = bf.readLine();
 
-                        if(answer != null && !curField.options.contains(answer))
-                            answer = null;
-                    }
-                    if(curField.name.equals("size"))
-                        size = answer;
-                    if(curField.name.equals("toppings"))
-                        toppings = answer;
-                    if(curField.name.equals("crust"))
-                        crust = answer;
-                    if(curField.name.equals("count"))
-                        count = answer;
-                }else{
-                    String confirm_answer = null;
-                    while(confirm_answer == null){
-                        // what the system confirms with
-                        System.out.println(curField.prompt.replaceAll("<insert_item>", answer));
-                        // the answer you get from the user
-                        confirm_answer = bf.readLine();
-                        if(confirm_answer.toLowerCase().equals("no")){
-                            // get back to the last field
-                            i -= 2;
-                            break;
-                        }
+                    if (answer != null
+                            && !curField.options.contains(answer))
+                        answer = null;
+                }
+                if (curField.name.equals("size"))
+                    size = answer;
+                if (curField.name.equals("toppings"))
+                    toppings = answer;
+                if (curField.name.equals("crust"))
+                    crust = answer;
+                if (curField.name.equals("count"))
+                    count = answer;
+            } else {
+                String confirm_answer = null;
+                while (confirm_answer == null) {
+                    // what the system confirms with
+                    System.out.println(curField.prompt.replaceAll(
+                            "<insert_item>", answer));
+                    // the answer you get from the user
+                    confirm_answer = bf.readLine();
+                    if (confirm_answer.toLowerCase().equals("no")) {
+                        // get back to the last field
+                        i -= 2;
+                        break;
                     }
                 }
             }
-
-            if(count.equals("one"))
-                count = "1";
-            if(count.equals("two"))
-                count = "2";
-            if(count.equals("three"))
-                count = "3";
-
-            System.out.println("Fine. Your total is " + price(size, toppings, Integer.parseInt(count), crust) + " pounds. Your order will be ready shortly.");
-
         }
+
+        if (count.equals("one"))
+            count = "1";
+        if (count.equals("two"))
+            count = "2";
+        if (count.equals("three"))
+            count = "3";
+
+        System.out.println("Fine. Your total is "
+                + price(size, toppings, Integer.parseInt(count), crust)
+                + " pounds. Your order will be ready shortly.");
+
     }
 }
